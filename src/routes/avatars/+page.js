1b1dac55184c22719dc5bc0aa216eec35cf9186d
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-09-01 12:41:24 -0600

    update

--- src/routes/avatars/+page.js
+++ src/routes/avatars/+page.js
@@ -1,22 +1,24 @@
-//TODO: Install aaron-bond.better-comments to read comments more effectively.
-
 import { faker } from '@faker-js/faker';
 
 //! Input Properties:
 // This is the way to load props/data into your page from a module JS/TS file for +page.svelte
 // load() is the function initializer to load data (similiar to getServerSideProps())
-export const load = async ({}) => {
+export const load = async () => {
 	const Avatars = [...Array(50)].map((el) => {
+		const name = faker.name.fullName();
+		const image = `https://avatars.dicebear.com/api/adventurer/${name}.svg`;
+
 		return {
-			name: faker.name.fullName(),
-			image: `https://avatars.dicebear.com/api/adventurer/${faker.name.fullName()}.svg`
+			name,
+			image
 		};
-	});
+	});	
 
 	const singleAvatar = {
 		name: faker.name.fullName(),
 		image: faker.image.people(640, 480, true)
 	};
+
 	return { fakerData: Avatars, singleFakeData: singleAvatar };
 };
 
--- src/routes/avatars/[profile]/+page.svelte
+++ src/routes/avatars/[profile]/+page.svelte
@@ -3,9 +3,22 @@
 
 	// Svelte Tutorial -> Props: Default Values ✅
 	export let defaultValue = 'Hello World';
+
+	// Svelte Tutorial -> Logic: If Blocks ✅
+	let profile = { loggedIn: false };
+	const toggledState = () =>
+		profile.loggedIn ? (profile.loggedIn = false) : (profile.loggedIn = true);
 </script>
 
 <main>
 	<h1>{defaultValue}</h1>
 	<p>{data.avatarName}</p>
+
+	<!-- * Svelte Tutorial -> Logic: If Blocks ✅ -->
+	{#if profile.loggedIn}
+		<button on:click={toggledState}> Sign Out </button>
+		<!-- * Svelte Tutorial -> Logic: Else Blocks ✅ -->
+	{:else}
+		<button on:click={toggledState}> Sign In </button>
+	{/if}
 </main>
