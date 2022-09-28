d76ada3d61baaab4ea91da6019f24a35045d11e2
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-09-27 19:22:41 -0600

    updated avatar page for create new avatar func"

--- src/routes/avatars/+page.svelte
+++ src/routes/avatars/+page.svelte
@@ -17,13 +17,23 @@
 </script>
 
 <main class="avatarMain flex flex-col gap-4">
-	<div class="container w-full">
+	<div class="w-full flex justify-between items-center">
 		<h1 class="text-xl">{@html pageTitle}</h1>
+
+		<!-- Wire this up with form actions? -->
+		<div class="flex">
+			<input
+				type="text"
+				placeholder="New Avatar Name"
+				class="input input-bordered w-full max-w-xs"
+			/>
+			<button class="btn btn-success">+</button>
+		</div>
 	</div>
 
-	<div class="flex gap-px flex-wrap justify-center">
+	<div class="flex gap-4 flex-wrap justify-center">
 		{#each data.AvatarData as data (data.name)}
-			<a href={`/avatars/${data.name}`}>
+			<a href={`/avatars/${data.name}`} class="w-1/3">
 				<Avatar {...data} />
 			</a>
 		{/each}
@@ -33,10 +43,5 @@
 <style>
 	main {
 		padding: 1rem;
-	}
-
-	a {
-		width: 20em;
-		height: 20em;
 	}
 </style>
