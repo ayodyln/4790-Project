cac855ac08fcfbe4e1f01c4318591a45dd489044
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-10-08 12:27:38 -0600

    Updated Avatar UI

--- src/routes/avatars/+page.svelte
+++ src/routes/avatars/+page.svelte
@@ -30,25 +30,46 @@
 
 	let inputField;
 
+	$: toggleAlert = false;
+
 	async function newAvatar() {
-		const avatar = await NewFaker(inputField.value);
-		avatarArray = [avatar, ...avatarArray];
-		inputField.value = '';
+		if (inputField.value === '') {
+			toggleAlert = 'FAIL';
+		}
+
+		if (inputField.value !== '') {
+			const avatar = await NewFaker(inputField.value);
+			avatarArray = [avatar, ...avatarArray];
+			toggleAlert = 'SUCCESS';
+			inputField.value = '';
+		}
+
+		setTimeout(() => {
+			toggleAlert = false;
+		}, 3000);
 	}
 </script>
 
-<main class="avatarMain flex flex-col gap-4">
+<main class="relative avatarMain flex flex-col gap-4 w-full overflow-x-hidden">
 	<div class="w-full flex justify-between items-center">
 		<h1 class="text-xl">{avatarArray.length} {@html pageTitle}</h1>
 
-		<div class="flex">
-			<input
-				type="text"
-				placeholder="Full Name (First, Last)"
-				class="input input-bordered w-full max-w-xs"
-				bind:this={inputField}
-			/>
-			<button on:click={newAvatar} class="btn btn-success">+</button>
+		<div class="form-control">
+			<div class="input-group flex items-center">
+				<input
+					type="text"
+					placeholder="Search…"
+					class="input input-bordered input-md"
+					bind:this={inputField}
+				/>
+				<button class="btn btn-square flex justify-center items-center btn-md" on:click={newAvatar}>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fill-current w-4"
+						><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+						/></svg
+					>
+				</button>
+			</div>
 		</div>
 	</div>
 
@@ -59,6 +80,44 @@
 			</a>
 		{/each}
 	</div>
+
+	{#if toggleAlert === 'SUCCESS'}
+		<div class="alert alert-success shadow-lg fixed bottom-0 right-0 z-10 w-96 m-8">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="stroke-current flex-shrink-0 h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					><path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>Succesfully created a new avatar!</span>
+			</div>
+		</div>
+	{:else if toggleAlert === 'FAIL'}
+		<div class="alert alert-error shadow-lg fixed bottom-0 right-0 z-10 w-96 m-8">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="stroke-current flex-shrink-0 h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					><path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>Must provide a Avatar Name in input field!</span>
+			</div>
+		</div>
+	{/if}
 </main>
 
 <style>
--- src/routes/avatars/components/Avatar.svelte
+++ src/routes/avatars/components/Avatar.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <div class="card bg-base-300 shadow-xl">
-	<div class="card-body h-full w-full items-center">
-		<img src={image} alt={name} />
-		<h3 class="text-xl text-current">{name}</h3>
+	<div class="card-body h-full w-full gap-4">
+		<div class="flex w-56 h-56 sm:w-full">
+			<img src={image} alt={name} class="w-full h-full" />
+		</div>
+		<div class="h-12 flex justify-center items-center">
+			<h3 class="text-lg w-full text-center">{name}</h3>
+		</div>
 	</div>
 </div>
