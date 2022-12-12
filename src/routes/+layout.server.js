523681cc7a84b283f32f81cf500e772f7f62dea6
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-12-11 19:43:50 -0700

    testing vercel env

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,11 +1,36 @@
 import { Amplify } from 'aws-amplify'
-// import awsExports from '../aws-exports'
-import { aws_exports } from '$env/static/private'
+import { AWS_COGNITO_IDENTITY_POOL_ID } from '$env/static/private'
 
 export async function load() {
-	Amplify.configure(JSON.parse(aws_exports))
+	const db = Amplify.configure(awsExport)
+
+	if (!db)
+		return {
+			status: 500
+		}
 
 	return {
 		msg: 'Amplify Configured'
 	}
+}
+
+const awsExport = {
+	aws_project_region: 'us-east-1',
+	aws_cognito_identity_pool_id: AWS_COGNITO_IDENTITY_POOL_ID,
+	aws_cognito_region: 'us-east-1',
+	aws_user_pools_id: 'us-east-1_JWvpWgaER',
+	aws_user_pools_web_client_id: '5b24tn36bll2cgmg4bfgj5q7nk',
+	oauth: {},
+	aws_cognito_username_attributes: [],
+	aws_cognito_social_providers: [],
+	aws_cognito_signup_attributes: ['EMAIL'],
+	aws_cognito_mfa_configuration: 'OFF',
+	aws_cognito_mfa_types: ['SMS'],
+	aws_cognito_password_protection_settings: {
+		passwordPolicyMinLength: 8,
+		passwordPolicyCharacters: []
+	},
+	aws_cognito_verification_mechanisms: ['EMAIL'],
+	aws_user_files_s3_bucket: 'sveltekit-dgm3790-aws-storage-d5981726221052-staging',
+	aws_user_files_s3_bucket_region: 'us-east-1'
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import '../app.css'
 	import { theme } from '$lib/stores/stores'
 
-	// export let data
+	export let data
+
+	console.log(data)
 </script>
 
 <div class="flex h-screen min-h-screen max-height-auto flex-col" data-theme={$theme}>
