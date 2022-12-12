a4e1576b2f2ccbe2cd5bcb4e0db96e8211b966e2
ayodyln <78442693+ayodyln@users.noreply.github.com>
2022-12-11 20:18:32 -0700

    ENV Variables Changed

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,10 @@
 import { Amplify } from 'aws-amplify'
-import { AWS_COGNITO_IDENTITY_POOL_ID } from '$env/static/private'
+import {
+	AWS_COGNITO_IDENTITY_POOL_ID,
+	AWS_USER_POOLS_ID,
+	AWS_USER_POOLS_WEB_CLIENT_ID,
+	AWS_USER_FILES_S3_BUCKET
+} from '$env/static/private'
 
 export async function load() {
 	const db = Amplify.configure(awsExport)
@@ -18,8 +23,8 @@ const awsExport = {
 	aws_project_region: 'us-east-1',
 	aws_cognito_identity_pool_id: AWS_COGNITO_IDENTITY_POOL_ID,
 	aws_cognito_region: 'us-east-1',
-	aws_user_pools_id: 'us-east-1_JWvpWgaER',
-	aws_user_pools_web_client_id: '5b24tn36bll2cgmg4bfgj5q7nk',
+	aws_user_pools_id: AWS_USER_POOLS_ID,
+	aws_user_pools_web_client_id: AWS_USER_POOLS_WEB_CLIENT_ID,
 	oauth: {},
 	aws_cognito_username_attributes: [],
 	aws_cognito_social_providers: [],
@@ -31,6 +36,6 @@ const awsExport = {
 		passwordPolicyCharacters: []
 	},
 	aws_cognito_verification_mechanisms: ['EMAIL'],
-	aws_user_files_s3_bucket: 'sveltekit-dgm3790-aws-storage-d5981726221052-staging',
+	aws_user_files_s3_bucket: AWS_USER_FILES_S3_BUCKET,
 	aws_user_files_s3_bucket_region: 'us-east-1'
 }
