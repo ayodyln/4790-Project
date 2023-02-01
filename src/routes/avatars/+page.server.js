32fc990dc97ad15cf90517efddfccf8363f59dbc
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-01 14:38:28 -0700

    testing removing api auth checker

--- src/routes/avatars/+page.server.js
+++ src/routes/avatars/+page.server.js
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { redirect, error } from '@sveltejs/kit'
+// import { redirect, error } from '@sveltejs/kit'
 
 let Avatar_Database = []
 
@@ -16,11 +16,11 @@ export const actions = {
 //! Input Properties:
 // This is the way to load props/data into your page from a module JS/TS file for +page.svelte
 // load() is the function initializer to load data (similiar to getServerSideProps())
-export const load = async ({ cookies }) => {
-	if (cookies.get('sessionID') === '' || !cookies.get('sessionID')) {
-		error(401, 'Not Logged In')
-		throw redirect(307, '/Auth')
-	}
+export const load = async () => {
+	// if (cookies.get('sessionID') === '' || !cookies.get('sessionID')) {
+	// 	error(401, 'Not Logged In')
+	// 	throw redirect(307, '/Auth')
+	// }
 
 	const Avatars = [...Array(50)].map((el, id) => {
 		const name = faker.name.fullName()
