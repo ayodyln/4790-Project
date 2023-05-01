6e3ce1e225e5d76a76c55da2a149ded33e898fa5
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-05-01 14:10:45 -0600

    testing layout theme error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,19 @@
 	import { Amplify, AuthModeStrategyType } from 'aws-amplify'
 	import awsconfig from '../aws-exports.js'
 	import ThemeChanger from '../lib/components/layout/ThemeChanger.svelte'
+	import { onMount } from 'svelte'
 	Amplify.configure({
 		...awsconfig,
 		DataStore: { authModeStrategyType: AuthModeStrategyType.MULTI_AUTH }
 	})
+
+	let currTheme = 'light'
+	onMount(() => {
+		currTheme = $theme ? 'light' : 'dark'
+	})
 </script>
 
-<div
-	class="flex h-auto min-h-screen max-height-auto flex-col"
-	data-theme={$theme ? 'light' : 'dark'}>
+<div class="flex h-auto min-h-screen max-height-auto flex-col" data-theme={currTheme}>
 	<slot />
 	<ThemeChanger />
 </div>
